02673ac081afef2a3ab5a91f987012b587ec171a
omkarpai <1998opai@gmail.com>
2022-07-25 17:17:09 +0530

    add modules generation

---
+++ server/src/lib/Zoho/Modules.ts
@@ -0,0 +1,23 @@
+enum ModulePermission {
+    ALL,
+    READ,
+    CREATE,
+    UPDATE,
+    DELETE,
+}
+
+class Module {
+    apiName: string;
+    scope: string;
+
+    constructor(apiName: string, permission: ModulePermission) {
+        this.apiName = apiName;
+        this.scope = `module.${apiName.toLowerCase()}.${ModulePermission[permission]}`;
+        if (!apiName) this.scope = `modules.${ModulePermission[ModulePermission.ALL]}`;
+    }
+}
+
+export class Modules {
+    public static readonly ALL = new Module("", ModulePermission.ALL);
+    public static readonly LEADS_ALL = new Module("Leads", ModulePermission.ALL);
+}
