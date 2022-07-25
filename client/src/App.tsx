5d6d328ccd165f54c32c032771d67d299f255c5c
omkarpai <1998opai@gmail.com>
2022-07-25 23:03:16 +0530

    add eslint

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import {
-  Routes,
-  Route,
-  BrowserRouter ,
-} from "react-router-dom";
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
-import './App.css';
-import Grant from './pages/grant/Grant';
-import Home from './pages/home/Home';
+import "./App.css";
+import Grant from "./pages/grant/Grant";
+import Home from "./pages/home/Home";
 
 function App() {
+
+
   
-  return (
-    <BrowserRouter>
-    <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/grant' element={<Grant/>}/>
-      </Routes>
-    </BrowserRouter>
-      
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/grant" element={<Grant />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
