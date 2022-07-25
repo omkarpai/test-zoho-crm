ec397f2713e59441cb5efb9047f1c228f5d1d363
omkarpai <1998opai@gmail.com>
2022-07-26 00:09:43 +0530

    fix eslintrc; add grant generation

--- client/src/pages/grant/Grant.tsx
+++ client/src/pages/grant/Grant.tsx
@@ -1,6 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSearchParams } from 'react-router-dom'
+
+class AccessGrant {
+    code: string | undefined
+
+    location: string | undefined
+
+    accountsServer: string | undefined
+
+    constructor(paramMap: Map<string, string>) {
+        this.code = paramMap.get('code')
+        this.location = paramMap.get('location')
+        this.accountsServer = paramMap.get('accounts-server')
+    }
+}
 
 function Grant() {
+    const [params] = useSearchParams()
+
+    useEffect(() => {
+        const paramMap = new Map()
+        params.forEach((value, key) => {
+            paramMap.set(key, value)
+        })
+        const ag = new AccessGrant(paramMap)
+        console.log(ag)
+    }, [params])
+
     return <div>Grant</div>
 }
 
