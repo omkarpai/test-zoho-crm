ff5818abf7378b9ed2acb7ac3d81350fa9b509b6
omkarpai <1998opai@gmail.com>
2022-07-25 17:16:34 +0530

    add port and uri to env ts

--- server/src/env.ts
+++ server/src/env.ts
@@ -2,5 +2,7 @@ require("dotenv").config();
 const ENV = {
     ZOHO_CLIENT_ID: process.env.ZOHO_CLIENT_ID,
     ZOHO_CLIENT_SECRET: process.env.ZOHO_CLIENT_SECRET,
+    PORT: process.env.PORT,
+    REDIRECT_URI: process.env.REDIRECT_URI,
 };
 export default ENV;
