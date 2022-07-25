e3cf33412d780b68a4a4ba454931702458932b78
omkarpai <1998opai@gmail.com>
2022-07-25 22:42:44 +0530

    add client with router

---
+++ client/src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  Routes,
+  Route,
+  BrowserRouter ,
+} from "react-router-dom";
+
+import './App.css';
+import Grant from './pages/grant/Grant';
+import Home from './pages/home/Home';
+
+function App() {
+  
+  return (
+    <BrowserRouter>
+    <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/grant' element={<Grant/>}/>
+      </Routes>
+    </BrowserRouter>
+      
+  );
+}
+
+export default App;
---
+++ client/src/pages/grant/Grant.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Grant() {
+  return (
+    <div>Grant</div>
+  )
+}
+
+export default Grant
