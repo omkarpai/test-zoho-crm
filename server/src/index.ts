f38f05b0ec07f7635176edd745be66b3f6b04293
omkarpai <1998opai@gmail.com>
2022-07-25 17:16:44 +0530

    add auth controller

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,2 +1,14 @@
 import ENV from "./env";
-console.log(ENV);
+import express from "express";
+import { authRouter } from "./routes/AuthRouter";
+
+const app = express();
+
+app.get("/", function (req, res) {
+    res.send("Hello World");
+});
+
+app.use("/auth", authRouter);
+
+app.listen(ENV.PORT);
+console.log("Listening on ", ENV.PORT);
