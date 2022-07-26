1a53102a7040267037e2e1e8bb727b0fadb1ac02
omkarpai <1998opai@gmail.com>
2022-07-26 22:10:36 +0530

    change env ts to return specific types

--- server/src/env.ts
+++ server/src/env.ts
@@ -1,8 +1,25 @@
 require("dotenv").config();
+
+const getClientId = () => {
+    return process.env.ZOHO_CLIENT_ID ? process.env.ZOHO_CLIENT_ID : "";
+};
+
+const getClientSecret = () => {
+    return process.env.ZOHO_CLIENT_SECRET ? process.env.ZOHO_CLIENT_SECRET : "";
+};
+
+const getPort = () => {
+    return process.env.PORT ? process.env.PORT : 4545;
+};
+
+const getRedirectURI = () => {
+    return process.env.REDIRECT_URI ? process.env.REDIRECT_URI : "";
+};
+
 const ENV = {
-    ZOHO_CLIENT_ID: process.env.ZOHO_CLIENT_ID,
-    ZOHO_CLIENT_SECRET: process.env.ZOHO_CLIENT_SECRET,
-    PORT: process.env.PORT,
-    REDIRECT_URI: process.env.REDIRECT_URI,
+    ZOHO_CLIENT_ID: getClientId(),
+    ZOHO_CLIENT_SECRET: getClientSecret(),
+    PORT: getPort(),
+    REDIRECT_URI: getRedirectURI(),
 };
 export default ENV;
