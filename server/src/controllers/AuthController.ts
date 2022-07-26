14988cf51d5ee6d8d1abf875fb401373910e95d4
omkarpai <1998opai@gmail.com>
2022-07-26 22:10:47 +0530

    add method to get access token

--- server/src/controllers/AuthController.ts
+++ server/src/controllers/AuthController.ts
@@ -1,10 +1,16 @@
-import express from "express";
-import { getLoginLink } from "../lib/Zoho/ZohoLib";
+import express, { Request, Response } from "express";
+import { AccessGrant, getLoginLink, getZohoAccessToken } from "../lib/Zoho/ZohoLib";
 
-const generateLoginLink = (req: express.Request, res: express.Response) => {
+const generateLoginLink = (req: Request, res: Response) => {
     const link = getLoginLink();
     res.status(200);
     res.send({ link });
 };
 
-export { generateLoginLink };
+const getAccessToken = async (req: Request, res: Response) => {
+    const ag: AccessGrant = req.query as any;
+    const at = await getZohoAccessToken(ag);
+    res.send(at);
+};
+
+export { generateLoginLink, getAccessToken };
