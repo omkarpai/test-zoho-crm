7e61c49c168e0ccc033aaed5d3e50da620fe740d
omkarpai <1998opai@gmail.com>
2022-10-12 20:25:23 +0530

    read version in env

--- server/src/env.ts
+++ server/src/env.ts
@@ -13,12 +13,17 @@ const getPort = () => {
 };
 
 const getRedirectURI = () => {
-    return process.env.REDIRECT_URI ? process.env.REDIRECT_URI : "";
+    return process.env.REDIRECT_URI ? process.env.REDIRECT_URI : "http://localhost:3000/grant";
+};
+
+const getZohoApiVersion = () => {
+    return process.env.ZOHO_API_VERSION ? process.env.ZOHO_API_VERSION : "v3";
 };
 
 const ENV = {
     ZOHO_CLIENT_ID: getClientId(),
     ZOHO_CLIENT_SECRET: getClientSecret(),
+    ZOHO_API_VERSION: getZohoApiVersion(),
     PORT: getPort(),
     REDIRECT_URI: getRedirectURI(),
 };
